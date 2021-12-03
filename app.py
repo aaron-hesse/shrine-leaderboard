49d1813f0c114df8d981924fb314e345e0a41697
aaron-hesse <aaronhesse@protonmail.com>
2021-12-02 20:55:38 -0800

    Testing database changes.

--- app.py
+++ app.py
@@ -13,9 +13,6 @@ app = Flask(__name__)
 def index():
     return "Hello World!"
 
-# presumably the game client just calls a single api endpoint with all the information.
-# then write the information to the database (postgres)
-
 @app.route("/recordGameResults")
 def recordGameResults():
     
@@ -24,14 +21,22 @@ def recordGameResults():
     player2Id = int(request.args.get('player2Id'))
     winningPlayerId = int(request.args.get('winningPlayerId'))
 
-    msg = "gameid type is: " + str(type(gameId) is str)
-
-    
-
-    #msg = "(recordGameResults): INSERTING the following information: " + gameId + " " + " player1Id: " + player1Id + " player2Id: " + player2Id + " winningPlayerId: " + winningPlayerId
     cur = conn.cursor()
     cur.execute("INSERT INTO gameRecords (gameId, player1Id, player2Id, winningPlayerId) VALUES (%s,%s,%s,%s)", (gameId,player1Id,player2Id,winningPlayerId) )
     conn.commit()
     conn.close()
 
-    return msg
+    return "Wrote the game info into the table."
+
+@app.route("/getGameResults")
+def getGameResults():
+
+    gameId = int(request.args.get('gameId'))
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM gameRecords WHERE gameId=%s", gameId)
+    gameRecord = cur.fetchone()
+
+
+    return "gameId: " + gameRecord[0] + " player1Id: " + gameRecord[1]
+
