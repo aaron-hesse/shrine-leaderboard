f9c2ea18426d583093d5aaa022c96b733920cedf
aaron-hesse <aaronhesse@protonmail.com>
2021-12-02 20:09:45 -0800

    Testing database changes.

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def recordGameResults():
 
     #msg = "(recordGameResults): INSERTING the following information: " + gameId + " " + " player1Id: " + player1Id + " player2Id: " + player2Id + " winningPlayerId: " + winningPlayerId
     cur = conn.cursor()
-    cur.execute("INSERT INTO gameRecords (gameId, player1Id, player2Id, winningPlayerId) VALUES (%i,%i,%i,%i)", (gameId,player1Id,player2Id,winningPlayerId) )
+    cur.execute("INSERT INTO gameRecords (gameId, player1Id, player2Id, winningPlayerId) VALUES (%s,%s,%s,%s)", (gameId,player1Id,player2Id,winningPlayerId) )
     #conn.commit()
     #conn.close()
 
