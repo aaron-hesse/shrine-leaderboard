0ecf467f46cea9a9736047b2bc580afa2b1d0a1c
aaron-hesse <aaronhesse@protonmail.com>
2021-12-03 10:22:34 -0800

    testing json changes

--- app.py
+++ app.py
@@ -69,4 +69,4 @@ def getPlayerResults():
     for game in gameRecords:   
         allGameRecords.append("{" + str(game[0]) + "}")
     
-    return json.dumps(gameRecords[0])
+    return jsonify(gameRecords[0])
