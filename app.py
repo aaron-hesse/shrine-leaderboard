e5a7cbd44351d0ef351afea79f95dee1a93ee87b
aaron-hesse <aaronhesse@protonmail.com>
2021-12-03 12:13:44 -0800

    Trying to handle duplicate entry check for game data.

--- app.py
+++ app.py
@@ -24,65 +24,58 @@ def index():
 @app.route("/recordGameResults")
 def recordGameResults():
     
-    gameId = int(request.args.get('gameId'))
-    player1Id = int(request.args.get('player1Id'))
-    player2Id = int(request.args.get('player2Id'))
-    winningPlayerId = int(request.args.get('winningPlayerId'))
+    game_id = int(request.args.get('gameId'))
+    player1_id = int(request.args.get('player1Id'))
+    player2_id = int(request.args.get('player2Id'))
+    winning_player_id = int(request.args.get('winningPlayerId'))
 
     cur = conn.cursor()
-    cur.execute("INSERT INTO gameRecords (gameId, player1Id, player2Id, winningPlayerId) VALUES (%s,%s,%s,%s)", (gameId,player1Id,player2Id,winningPlayerId) )
-    conn.commit()
+    try:
+        cur.execute("INSERT INTO gameRecords (gameId, player1Id, player2Id, winningPlayerId) VALUES (%s,%s,%s,%s)", (gameId,player1Id,player2Id,winningPlayerId) )
+        conn.commit()
+    except UniqueViolation:
+        return "Game results with that ID already exist."
 
     return jsonify(
-        gameid=gameId,
-        player1id=player1Id,
-        player2id=player2Id,
-        winningplayerid=winningPlayerId
+        gameId=gameId,
+        player1Id=player1Id,
+        player2Id=player2Id,
+        winningPlayerId=winningPlayerId
     )
 
 @app.route("/getGameResults")
 def getGameResults():
 
-    gameIdStr = request.args.get('gameId')
+    game_id_str = request.args.get('gameId')
 
     cur = conn.cursor()
-    cur.execute("SELECT * FROM gameRecords WHERE gameId=%s", gameIdStr)
-    gameRecord = cur.fetchone()
+    cur.execute("SELECT * FROM gameRecords WHERE gameId=%s", game_id_str)
+    game_record = cur.fetchone()
 
     return jsonify(
-        gameid=gameRecord[0],
-        player1id=gameRecord[1],
-        player2id=gameRecord[2],
-        winningplayerid=gameRecord[3]
+        gameId=game_record[0],
+        player1Id=game_record[1],
+        player2Id=game_record[2],
+        winningPlayerId=game_record[3]
     )
 
 @app.route("/getPlayerResults")
 def getPlayerResults():
 
-    playerIdStr = request.args.get('playerId')
+    player_id_str = request.args.get('playerId')
 
     cur = conn.cursor()
-    cur.execute("SELECT * FROM gameRecords WHERE player1Id=%s OR player2Id=%s", (playerIdStr,playerIdStr))
-    gameRecords = cur.fetchall()
+    cur.execute("SELECT * FROM gameRecords WHERE player1Id=%s OR player2Id=%s", (player_id_str,player_id_str))
+    game_records = cur.fetchall()
 
-    #allGameRecords = ""
-    #allGameRecords += "{"
-    #for game in gameRecords:  
-      #  allGameRecords += "{"
-      #  allGameRecords += " 'gameid': " + str(game[0]) + ","
-     #   allGameRecords += " 'player1id': " + str(game[1]) + ","
-    #    allGameRecords += " 'player2id': " + str(game[2]) + ","
-    #    allGameRecords += " 'winningPlayerid': " + str(game[3])
-    #    allGameRecords += "}"
-    #allGameRecords += "}"
-
-    allGameRecords = []
-    game = {}
-    for gameR in gameRecords:
+    all_game_records = []
+    
+    for gameR in game_records:
+        game = {}
         game['gameid'] = str(gameR[0])
         game['player1id'] = str(gameR[1])
         game['player2id'] = str(gameR[2])
         game['winningplayerid'] = str(gameR[3])
-        allGameRecords.append(game)
+        all_game_records.append(game)
     
-    return jsonify(allGameRecords)
+    return jsonify(all_game_records)
