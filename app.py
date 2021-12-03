04fc9dff23e5d9c3929d27e795ebc32c389a7126
aaron-hesse <aaronhesse@protonmail.com>
2021-12-02 17:40:32 -0800

    testing changes to new endpoint

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 def index():
     return "Hello World!"
 
+# presumably the game client just calls a single api endpoint with all the information.
+
 @app.route("/recordGameResults")
 def recordGameResults():
-    return "recordGameResults"
+    gameId = request.args.get('gameId')
+    player1Name = request.args.get('player1Name')
+    player2Name = request.args.get('player2Name')
+    winningPlayer = request.args.get('winningPlayer')
+    return "recordGameResults: " + gameId + " " + " player1Name: " + player1Name
