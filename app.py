9c71bf358790dedc24b53b013da9835f7390060c
aaron-hesse <aaronhesse@protonmail.com>
2021-12-02 18:51:48 -0800

    Testing database changes.

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import psycopg2
 
 DATABASE_URL = os.environ['DATABASE_URL']
 
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
 app = Flask(__name__)
 
 @app.route("/")
@@ -14,11 +12,27 @@ def index():
     return "Hello World!"
 
 # presumably the game client just calls a single api endpoint with all the information.
+# then write the information to the database (postgres)
 
 @app.route("/recordGameResults")
 def recordGameResults():
+    
     gameId = request.args.get('gameId')
     player1Id = request.args.get('player1Id')
     player2Id = request.args.get('player2Id')
     winningPlayerId = request.args.get('winningPlayerId')
+
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+    try:
+        cur = conn.cursor()
+        cur.execute("INSERT INTO gameResults (gameId,player1Id,player2Id,winningPlayerId) VALUES (?,?,?,?)", (gameId,player1Id,player2Id,winningPlayerId) )
+        conn.commit()
+    except:
+        conn.rollback()
+        msg = "error in insert operation"
+    
+    finally:
+        conn.close()
+
     return "recordGameResults: " + gameId + " " + " player1Id: " + player1Id + " player2Id: " + player2Id + " winningPlayerId: " + winningPlayerId
