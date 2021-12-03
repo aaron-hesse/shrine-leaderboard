9a1dfd16b99d01a681ff29a15b931a54788734e0
aaron-hesse <aaronhesse@protonmail.com>
2021-12-02 19:45:04 -0800

    Testing database changes.

--- app.py
+++ app.py
@@ -22,11 +22,13 @@ def recordGameResults():
     player2Id = request.args.get('player2Id')
     winningPlayerId = request.args.get('winningPlayerId')
 
+    msg = type(gameId)
+
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
     msg = "(recordGameResults): INSERTING the following information: " + gameId + " " + " player1Id: " + player1Id + " player2Id: " + player2Id + " winningPlayerId: " + winningPlayerId
     cur = conn.cursor()
-    cur.execute("INSERT INTO gameRecords (gameId,player1Id,player2Id,winningPlayerId) VALUES (?,?,?,?)", (gameId,player1Id,player2Id,winningPlayerId) )
+    cur.execute("INSERT INTO gameRecords (gameId, player1Id, player2Id, winningPlayerId) VALUES (?,?,?,?)", (gameId,player1Id,player2Id,winningPlayerId) )
     conn.commit()
     conn.close()
 
